d650ed0962e43530d0eb167140a6a7d3689af2c7
LuigiGrimaldi <grimaldi1110@gmail.com>
2023-03-13 16:56:39 +0100

    array commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// Creo un array con le mail valide per accedere
+const mails = [
+    'luizao@favelas.com',
+    'frosolone@company.com'
+    'unduetrestella@play.com'
+];
+
